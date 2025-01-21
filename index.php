955e59c48963feea2d8b7767c732d93bfbbdee0c
Mattia Bergamelli <mattia.bergamelli@net-target.it>
2025-01-21 16:16:04 +0100

    SQLSRV First Try

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo "Hello World!";
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sql-server-rossotech.database.windows.net,1433; Database = rossotech-azure", "mattia", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "mattia", "pwd" => "{your_password_here}", "Database" => "rossotech-azure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:sql-server-rossotech.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
